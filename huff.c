ca9275fb80b6f5fb07300759d53ad40124e88008
VitoriaO <mvom@ic.ufal.br>
2016-09-21 17:51:17 -0300

    Modificado huuf.c, add compressed.huff, testado

--- huff.c
+++ huff.c
@@ -3,6 +3,7 @@
 #include <math.h>
 char tree[20];
 int i;
+
 int binary_decimal(long int binary)
 {
     int decimal = 0, i = 0, aux;
@@ -55,17 +56,18 @@ void print_pre_order(Huffman_tree *ht)
 Huffman_tree* add(Huffman_tree *ht)
 {
 
-	if(tree[i] != '*')
+    if(tree[i] == '*')
     {
-       ht = create_tree(tree[i], NULL, NULL);
-       i++;
-       ht -> left = add(ht -> left);
-       ht -> right = add(ht -> right);
+        ht = create_tree(tree[i], NULL, NULL);
+        i++;
+        ht -> left = add(ht -> left);
+        ht -> right = add(ht -> right);
     }
     else
     {
-    	i++;
-    	return NULL;
+        ht = create_tree(tree[i], NULL, NULL);
+        i++;
+        return ht;
     }
     return ht;
 }
@@ -74,79 +76,79 @@ Huffman_tree* add(Huffman_tree *ht)
 int main ()
 {
 
-	FILE *compressed;
-	long lSize;
-	char *buffer;
-	size_t result;
-	char aux[14];
-	int garbage, tree_size;
-	int j;
+    FILE *compressed;
+    long lSize;
+    char *buffer;
+    size_t result;
+    char aux[14];
+    int garbage, tree_size;
+    int j;
     Huffman_tree *ht = create_empty();
 
-	// Here the file is open
-	compressed = fopen ("compressed.huff", "rb");
+    // Here the file is open
+    compressed = fopen ("compressed.huff", "rb");
 
-	if (compressed == NULL)
-	{
-	printf("Não tem arquivo pra ser extraido!\n");
-	exit (1);
-	}
+    if (compressed == NULL)
+    {
+        printf("Não tem arquivo pra ser extraido!\n");
+        exit (1);
+    }
 
-	//Analizar essa parte e ver o q faz
-	// obtain file size:
-	fseek (compressed , 0 , SEEK_END);
-	lSize = ftell(compressed);
-	rewind(compressed);
+    //Analizar essa parte e ver o q faz
+    // obtain file size:
+    fseek (compressed, 0, SEEK_END);
+    lSize = ftell(compressed);
+    rewind(compressed);
 
-	// allocate memory to contain the whole file:
-	buffer = (char*) malloc (sizeof(char)*lSize);
-	if (buffer == NULL)
-	{
-		printf("Memory error\n");
-		exit (2);
-	}
+    // allocate memory to contain the whole file:
+    buffer = (char*) malloc (sizeof(char)*lSize);
+    if (buffer == NULL)
+    {
+        printf("Memory error\n");
+        exit (2);
+    }
 
-	// copy the file into the buffer:
-	result = fread (buffer, 1, lSize, compressed);
-	if (result != lSize)
-	{
-		fputs ("Reading error",stderr);
-		exit (3);
-	}
+    // copy the file into the buffer:
+    result = fread (buffer, 1, lSize, compressed);
+    if (result != lSize)
+    {
+        fputs ("Reading error",stderr);
+        exit (3);
+    }
 
-	//Here we catch the garbage size:
-	for(i = 0; i < 3; i++)
-	{
+    //Here we catch the garbage size:
+    for(i = 0; i < 3; i++)
+    {
         aux[i] = buffer[i];
-	}
-	aux[3] = '\0';
-	garbage = atoi(aux);
-	garbage = binary_decimal(garbage);
-	printf("%d\n", garbage);
+    }
+    aux[3] = '\0';
+    garbage = atoi(aux);
+    garbage = binary_decimal(garbage);
+    printf("%d\n", garbage);
 
 
-	//Here we find the tree size
-	for( i = 3, j = 0; i < 16; i++, j++)
-	{
-		aux[j] = buffer[i];
-	}
-	aux[16] = '\0';
-	tree_size = atoi(aux);
-	tree_size = binary_decimal(tree_size);
-	printf("%d\n", tree_size);
+    //Here we find the tree size
+    for( i = 3, j = 0; i < 16; i++, j++)
+    {
+        aux[j] = buffer[i];
+    }
+    aux[16] = '\0';
+    tree_size = atoi(aux);
+    tree_size = binary_decimal(tree_size);
+    printf("%d\n", tree_size);
 
-	//Here we find the tree itself
-	for(i = 16, j = 0; i < (16 + tree_size); i++, j++)
-	{
-		tree[j] = buffer[i];
-	}
-	tree[i] = '\0';
-	printf("%s\n", tree);
-	i = 0;
-	ht = add(ht);
-	print_pre_order(ht);
+    //Here we find the tree itself
+    for(i = 16, j = 0; i < (16 + tree_size); i++, j++)
+    {
+        tree[j] = buffer[i];
+    }
+    tree[j] = '\0';
+    printf("%s\n", tree);
+    i = 0;
+    ht = add(ht);
+    print_pre_order(ht);
 
-	fclose (compressed);
-	free (buffer);
-	return 0;
+    fclose (compressed);
+    free (buffer);
+    return 0;
 }
