dc2a406b538796dc7a8e4b96309eb7009ef1ab3f
glauberrrbraga <glauberrrbraga@hotmail.com>
2016-09-24 16:06:39 -0300

    Add files via upload

--- huff.c
+++ huff.c
@@ -11,7 +11,7 @@ int binary_decimal(long int binary)
     {
         aux = (binary % 10);
         binary /= 10;
-        decimal += aux * pow(2, i);
+        decimal += aux * (i*i);
         i++;
     }
     return decimal;
@@ -72,77 +72,85 @@ Huffman_tree* add(Huffman_tree *ht)
     return ht;
 }
 
-
-int main ()
+int main () 
 {
 
-    FILE *compressed;
-    long lSize;
-    char *buffer;
-    size_t result;
-    char aux[14];
-    int garbage, tree_size;
-    int j;
-    Huffman_tree *ht = create_empty();
+	//Criar um arquivo formato FILE que vai receber o arquivo comprimido
+	FILE *compressed;
+	long lSize;
+	char *buffer;
+	//O buffer vai armazenar o arquivo temporariamente em formato de string 
+	size_t result;
+	char aux[14];
+	int garbage, tree_size;
+	int j;
+	Huffman_tree *ht = create_empty();
 
-    // Here the file is open
-    compressed = fopen ("compressed.huff", "rb");
+	// Abre o arquivo 'compressed.huff' em forma de binario (rb)
+	compressed = fopen ("compressed.huff", "rb");
 
-    if (compressed == NULL)
-    {
-        printf("Não tem arquivo pra ser extraido!\n");
-        exit (1);
-    }
-
-    //Analizar essa parte e ver o q faz
-    // obtain file size:
-    fseek (compressed, 0, SEEK_END);
-    lSize = ftell(compressed);
-    rewind(compressed);
-
-    // allocate memory to contain the whole file:
-    buffer = (char*) malloc (sizeof(char)*lSize);
-    if (buffer == NULL)
-    {
-        printf("Memory error\n");
-        exit (2);
-    }
-
-    // copy the file into the buffer:
-    result = fread (buffer, 1, lSize, compressed);
-    if (result != lSize)
-    {
-        fputs ("Reading error",stderr);
-        exit (3);
-    }
-
-    //Here we catch the garbage size:
-    for(i = 0; i < 3; i++)
-    {
-        aux[i] = buffer[i];
-    }
-    aux[3] = '\0';
-    garbage = atoi(aux);
-    garbage = binary_decimal(garbage);
-    printf("%d\n", garbage);
+	//Se o arquivo apontar para nulo, então ele estara vazio.
+	if (compressed == NULL) 
+	{
+		printf("Não tem arquivo pra ser extraido!\n");
+		exit (1);
+	}
 
 
-    //Here we find the tree size
-    for( i = 3, j = 0; i < 16; i++, j++)
-    {
-        aux[j] = buffer[i];
-    }
-    aux[16] = '\0';
-    tree_size = atoi(aux);
-    tree_size = binary_decimal(tree_size);
-    printf("%d\n", tree_size);
+	// Obtem o tamanho do arquivo indo do byte do inicio do arquivo até o ultimo
+	fseek (compressed , 0 , SEEK_END);
+	//ftell: Retorna o numero de bytes a partir do inicio do arquivo em arquivos binarios
+	lSize = ftell(compressed);
+	// Define o arquivo para o inicio do arquivo
+	rewind(compressed);
 
-    //Here we find the tree itself
-    for(i = 16, j = 0; i < (16 + tree_size); i++, j++)
-    {
-        tree[j] = buffer[i];
-    }
-    tree[j] = '\0';
+	// Aloca memoria do tamanho do arquivo para criar o buffer
+	buffer = (char*) malloc (sizeof(char)*lSize);
+	if (buffer == NULL)
+	{
+		printf("Erro de memoria\n"); 
+		exit (2);
+	}
+
+	//Copia o arquivo para o buffer
+	/*(O ponteiro para o bloco de memoria com o tamanho do arquivo, o tamanho em binario de cada elemento, 
+	o numero de elementos, o arquivo de origem)*/
+	result = fread (buffer, 1, lSize, compressed);
+	/*Result é o tamanho de arquivos lidos com sucesso. Se o numero de arquivos lidos com sucesso for diferente 
+	do tamanho da string, ocorreu um erro na leitura */
+	if (result != lSize) 
+	{
+		fputs ("Erro de leitura\n",stderr); 
+		exit (3);
+	}
+
+	//Here we catch the garbage size:
+	for(i = 0; i < 3; i++)
+	{
+		aux[i] = buffer[i];
+	}
+	aux[3] = '\0';
+	garbage = atoi(aux);
+	garbage = binary_decimal(garbage);
+	printf("%d\n", garbage);
+
+
+	//Here we find the tree size
+	for(i = 3, j = 0; i < 16; i++, j++)
+	{
+		aux[j] = buffer[i];
+	}
+	aux[16] = '\0';
+	tree_size = atoi(aux);
+	tree_size = binary_decimal(tree_size);
+	printf("%d\n", tree_size);
+	
+	//Here we find the tree itself
+	for(i = 16, j = 0; i < (16 + tree_size); i++, j++)
+	{
+		tree[j] = buffer[i];
+	}
+	tree[j] = '\0';
     printf("%s\n", tree);
     i = 0;
     ht = add(ht);
