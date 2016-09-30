f174cb2c9a7cfd690ebcd3c3e2a017ce20f2a663
VitoriaO <mvom@ic.ufal.br>
2016-09-30 20:32:28 -0300

    Add files via upload

--- huffnode.c
+++ huffnode.c
@@ -1,8 +1,8 @@
 /*
- * priority_queue.c
+ * priorityqueue.c
  *
- *  Created on: 23 de set de 2016
- *      Author: leony
+ *  Created on: 25 de set de 2016
+ *      Author: vitor_000
  */
 
 #ifndef PRIORITY_QUEUE_C_
@@ -10,107 +10,179 @@
 
 #include "huffnode.h"
 
-struct node{
-	char item;
+typedef struct node
+{
+	unsigned char item;
 	int frequency;
 	Node *next;
 	Node *left;
 	Node *right;
-};
+}Node;
 
-struct priority_queue{
+typedef struct pqueue
+{
 	Node *first;
 	Node *last;
-};
+}Priority_Queue;
 
-Node *create_node(char carac, int freq){
-	Node *new_node = (Node*) malloc (sizeof(Node));
-	if (carac == '*') new_node->item = '\*';
-	else new_node->item = carac;
+Node* create_node(unsigned char carac, int freq)
+{
+	Node* new_node = (Node*) malloc(sizeof(Node));
+
+	new_node->item = carac;
 	new_node->frequency = freq;
-	new_node->next = NULL;
+
 	new_node->left = NULL;
+	new_node->next = NULL;
 	new_node->right = NULL;
+
 	return new_node;
 }
 
-Priority_Queue *create_priority_queue(){
-	Priority_Queue *new_pq = (Priority_Queue*)malloc(sizeof(Priority_Queue));
+Priority_Queue* create_priority_queue()
+{
+	Priority_Queue *new_pq = (Priority_Queue*) malloc(sizeof(Priority_Queue));
 	new_pq->first = NULL;
 	new_pq->last = NULL;
+
 	return new_pq;
 }
 
-int is_unique(Priority_Queue *pq){
+int is_unique(Priority_Queue *pq)
+{
 	return (pq->first->next == NULL);
 }
 
-void enqueue_sorted(Priority_Queue *pq, Node *node){
-	if(pq->first == NULL) {
+void enqueue_sorted(Priority_Queue *pq, Node *node)
+{
+	if(pq->first == NULL)
+	{
 		pq->first = node;
 		pq->last = node;
-	} else if(node->frequency <= pq->first->frequency) {
+	}
+	else if (node->frequency <= pq->first->frequency)
+	{
 		node->next = pq->first;
 		pq->first = node;
-	} else if(node->frequency > pq->last->frequency) {
+	}
+	else if (node->frequency > pq->last->frequency)
+	{
 		pq->last->next = node;
 		pq->last = node;
-	} else {
-		Node *aux = pq->first;
-		while(node->frequency > aux->next->frequency && aux->next != NULL){
+	}
+	else
+	{
+		Node *aux;
+
+		aux = pq->first;
+		while (node->frequency > aux->next->frequency && aux->next != NULL)
+		{
 			aux = aux->next;
 		}
+
 		node->next = aux->next;
 		aux->next = node;
 	}
 }
 
-void print_queue(Priority_Queue *pq){
-	Node *aux;
-	for(aux = pq->first; aux != NULL; aux = aux->next){
-		printf("%c %d\n", aux->item, aux->frequency);
+void print_queue(Priority_Queue *pq)
+{
+	if(pq != NULL)
+	{
+		printf("%c %d\n", pq->first->item, pq->first->frequency);
 	}
 }
 
-Node *dequeue (Priority_Queue *pq){
-	if(pq->first != NULL){
-		Node *dequeued = pq->first;
+Node* dequeue(Priority_Queue *pq)
+{
+	if(pq->first != NULL)
+	{
+		Node *dequeued;
+
+		dequeued= pq->first;
 		pq->first = pq->first->next;
 		dequeued->next = NULL;
+
 		return dequeued;
 	}
-	else return NULL;
+	else
+	{
+		return NULL;
+	}
 }
 
-Node *build_tree (Priority_Queue *pq){
-	while (!is_unique(pq)){
+Node* build_tree(Priority_Queue *pq)
+{
+	while(!is_unique(pq))
+	{
 		Node *aux1 = dequeue(pq);
 		Node *aux2 = dequeue(pq);
+
 		int freq = aux1->frequency + aux2->frequency;
-		Node *new = (Node*) malloc (sizeof(Node));
-		new->frequency = freq;
-		new->item = '*';
-		new->left = aux1;
-		new->right = aux2;
-		new->next = NULL;
-		enqueue_sorted(pq,new);
+
+		Node *new_n = (Node*) malloc(sizeof(Node));
+
+		new_n->frequency = freq;
+		new_n->item = '*';
+		new_n->left = aux1;
+		new_n->next = aux2;
+
+		enqueue_sorted(pq, new_n);
 	}
+
 	return pq->first;
 }
 
-void print_tree_pre_order(Node *root, FILE *arq){
-	if(root != NULL){
-		fprintf(arq, "%c", root->item);
+void print_tree_pre_order(Node *root, FILE *arq)
+{
+	if(root != NULL)
+	{
+		if(root->item == '*' && root->left == NULL && root->right == NULL)
+		{
+			fprintf(arq, "%c%c", 92, 42);
+		}
+		else
+		{
+			fprintf(arq, "%c", root->item);
+		}
 		print_tree_pre_order(root->left, arq);
 		print_tree_pre_order(root->right, arq);
 	}
 }
 
-int tree_size(Node *root){
-	if(root == NULL) return 0;
-	else{
-		int size = 1 + tree_size(root->left) + tree_size(root->right);
-		return size;
+int tree_size(Node *root)
+{
+	if(root == NULL)
+	{
+		return 0;
+	}
+	else
+	{
+		return (1 + tree_size(root->left) + tree_size(root->right));
+	}
+}
+
+int is_leaf(Node *node)
+{
+	return (node->left == NULL && node->right == NULL);
+}
+
+void funcao(HashHuff *h, Node *root, unsigned char *c)
+{
+	if(root != NULL)
+	{
+		if(is_leaf(root))
+		{
+			put( h, root->item, c);
+			c = c - 1;
+		}
+		else
+		{
+			c[0] = 0;
+			funcao( h, root->left, (c+1));
+			c[0] = 1;
+			funcao( h, root->right, (c+1));
+		}
 	}
 }
 
--- huffnode.h
+++ huffnode.h
@@ -1,8 +1,8 @@
 /*
- * priority_queue.h
+ * priorityqueue.h
  *
- *  Created on: 24 de set de 2016
- *      Author: leony
+ *  Created on: 25 de set de 2016
+ *      Author: vitor_000
  */
 
 #ifndef HUFFNODE_H_
@@ -10,29 +10,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "Hash.h"
 
 typedef struct node Node;
 
-typedef struct priority_queue Priority_Queue;
+typedef struct pqueue Priority_Queue;
 
-Node *create_node(char carac, int freq);
+Node* create_node(unsigned char carac, int freq);
 
 Priority_Queue *create_priority_queue();
 
-int is_empty(Priority_Queue *pq);
-
 int is_unique(Priority_Queue *pq);
 
-void enqueue_sorted (Priority_Queue *pq, Node *node);
+void enqueue_sorted(Priority_Queue *pq, Node *node);
 
 void print_queue(Priority_Queue *pq);
 
-Node *dequeue (Priority_Queue *pq);
+Node* dequeue(Priority_Queue *pq);
 
-Node *build_tree (Priority_Queue *pq);
+Node* build_tree(Priority_Queue *pq);
 
 void print_tree_pre_order(Node *root, FILE *arq);
 
 int tree_size(Node *root);
+
+int is_leaf(Node *node);
+
+void funcao(HashHuff *h, Node *root, unsigned char *c);
 
 #endif /* HUFFNODE_H_ */
